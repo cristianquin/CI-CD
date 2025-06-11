298267b2c6e8559694ee2796c2bf9cfc48389ef5
cquinchiar <cquinchiar@unal.edu.co>
2025-06-11 17:46:45 -0500

    try again

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,7 +2,7 @@ variable "env_name" {}
 variable "instance_type" {}
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu Server 20.04 LTS (us-east-1)
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = var.instance_type
 
   tags = {
