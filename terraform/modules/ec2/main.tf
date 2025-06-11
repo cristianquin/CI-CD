d70daad3e3b6a42d9326a75b54a9a6b4f1b85b78
cquinchiar <cquinchiar@unal.edu.co>
2025-06-11 18:00:25 -0500

    new ami

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,7 +2,7 @@ variable "env_name" {}
 variable "instance_type" {}
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-020cba7c55df1f615"
   instance_type = var.instance_type
 
   tags = {
