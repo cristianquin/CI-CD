ad38f0488b55e2ad8744f9e2aa34ed9715b8f625
cquinchiar <cquinchiar@unal.edu.co>
2025-06-13 18:08:51 -0500

    letsgo

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -3,40 +3,6 @@ variable "env_name" {}
 resource "aws_security_group" "ec2_sg" {
   name        = "${var.env_name}-ec2-sg"
   description = "Permitir HTTP y SSH"
-  ingress = [
-    {
-      description = "SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  tags = {
-    Name = "${var.env_name}-ec2-sg"
-  }
-}
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.env_name}-sg"
-  description = "Security group para EC2"
-  vpc_id      = var.vpc_id # Solo si estás usando VPC explícitamente
 
   ingress {
     description = "Allow SSH"
@@ -63,7 +29,7 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   tags = {
-    Name = "${var.env_name}-sg"
+    Name = "${var.env_name}-ec2-sg"
   }
 }
 
